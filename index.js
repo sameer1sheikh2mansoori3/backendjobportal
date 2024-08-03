ce9bd0400290da1af8050186965a96d9522cebad
sameer1sheikh2mansoori3 <94507751+sameer1sheikh2mansoori3@users.noreply.github.com>
2024-08-03 13:46:48 +0530

    add

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'*',
-    credentials:true
+origin:'https://66ade5d9a22e2a4d36ea3857--relaxed-pie-541acb.netlify.app/', //or whatever port your frontend is using
+    credentials:true,            
+    optionSuccessStatus:200
+
 }
 
 app.use(cors(corsOptions));
