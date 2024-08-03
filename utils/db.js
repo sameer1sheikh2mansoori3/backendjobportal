3fe26ac6ccfb0f263d2ce3fd236a75e8c493be0e
sameer1sheikh2mansoori3 <94507751+sameer1sheikh2mansoori3@users.noreply.github.com>
2024-08-03 13:10:58 +0530

    backend first try

---
+++ utils/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            dbName:"jobportal"
+        });
+        console.log('mongodb connected successfully');
+    } catch (error) {
+        console.log(error);
+    }
+}
+export default connectDB;
